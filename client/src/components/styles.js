63b077c219602b866663e4e24ad347f96930031c
zaid <zaidhusseini@gmail.com>
2018-06-09 15:36:02 -0700

    Creates a styles page to store inline styling objects for React components

---
+++ client/src/components/styles.js
@@ -0,0 +1,37 @@
+
+const GridStyle = {
+  maxWidth: 350
+};
+
+/*=========================
+
+BUTTON STYLES
+
+==========================*/
+
+const LUMEOS_BLUE = '#25c4e2';
+
+const ButtonStyle = {
+  borderRadius: '30px',
+  marginTop: '10px',
+  marginBottom: '10px'
+} 
+
+const SignInStyle = {
+  marginTop: '1em',
+  color: LUMEOS_BLUE //Lumeos Teal Blue
+} 
+
+const SplashHeaderStyle = {
+  color: LUMEOS_BLUE
+}
+
+const LinkStyle = SplashHeaderStyle;
+
+
+export { GridStyle,
+         ButtonStyle,
+         SignInStyle,
+         SplashHeaderStyle,
+         LinkStyle
+       };

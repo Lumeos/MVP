1fc4b44a3f50f16ce46e25f8d45c2b2535b49c26
zaid <zaidhusseini@gmail.com>
2018-06-09 15:35:17 -0700

    Creates Login Page

---
+++ client/src/components/Login.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Form, Grid, Header, Image, Message, Segment, Divider, Icon } from 'semantic-ui-react'
+import { ButtonStyle, SignInStyle, LinkStyle } from './styles'
+
+
+const LoginForm = () => (
+  <div className='login-form'>
+    {/*
+      Heads up! The styles below are necessary for the correct render of this example.
+      You can do same with CSS, the main idea is that all the elements up to the `Grid`
+      below must have a height of 100%.
+    */}
+    <style>{`
+      body > div,
+      body > div > div,
+      body > div > div > div.login-form {
+        height: 100%;
+      }
+    `}</style>
+    <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+      <Grid.Column style={{ maxWidth: 350 }}>
+        <Image as={Link} to='/' src='static/images/lumeos_logo.png' size='medium' centered={true} />
+        <Header as='h2' textAlign='center'>
+          Log-in to your account
+        </Header>
+        <Form size='large'>
+            <Form.Input 
+              fluid 
+              icon='user' 
+              iconPosition='left' 
+              placeholder='E-mail address' />
+            <Form.Input
+              fluid
+              icon='lock'
+              iconPosition='left'
+              placeholder='Password'
+              type='password'
+            />
+            <Button 
+              fluid 
+              size='big'
+              style={ButtonStyle}
+              id="signin-button">
+              Login
+            </Button>
+            <Link to='#' style={{color: 'grey'}}> Forgot Password? </Link>
+        </Form>
+        <div>
+          <Divider horizontal>or</Divider>
+            <Button 
+              as={Link} 
+              to='/signup' 
+              fluid 
+              size='big' 
+              style={ButtonStyle} 
+              id="signup-button"
+              onMouseOver ={()=>console.log('hovered')}
+              onClick={()=>console.log('clicked')}>
+                Sign Up
+            </Button>
+            <Button fluid color='facebook'size='big' style={ButtonStyle}>
+              <Icon name='facebook' /> Facebook
+            </Button>
+            <Button fluid color='twitter' size='big' style={ButtonStyle}>
+              <Icon name='twitter' /> Twitter
+            </Button>
+        </div>
+      </Grid.Column>
+    </Grid>
+  </div>
+)
+
+export default LoginForm

d1856b19347493a9b645c5ee761278b0e6aeaa80
zaid <zaidhusseini@gmail.com>
2018-06-09 15:36:19 -0700

    Sets up index for React Routers

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import App from './components/App';
 import '../dist/styles.css';
 
-ReactDom.render(<App/>, document.getElementById('app'));
+ReactDom.render(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+  , document.getElementById('app'));

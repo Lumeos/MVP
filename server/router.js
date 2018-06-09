bb8019bbfb615c8d92ae98acf0f07425ea20ea74
zaid <zaidhusseini@gmail.com>
2018-06-09 15:35:04 -0700

    Updates router to send bundle.js file to all paths

--- server/router.js
+++ server/router.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const axios = require('axios');
+
 require('dotenv').load();
 
-router.get('/', (req, res)=>{
+router.get('*', (req, res)=>{
   //do stuff
+  res.sendFile(path.join(__dirname,'..', 'client','dist','/index.html'));
 });
 
 

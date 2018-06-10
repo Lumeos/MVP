f8f92e9f005b1effe0b91702fd740756c9904f33
zaid <zaidhusseini@gmail.com>
2018-06-09 20:10:27 -0700

    Adds Favicon

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-const router = require('./router.js')
+const router = require('./router.js');
+const favicon = require('serve-favicon');
+const path = require('path');
 const morgan = require('morgan');
 
 app.use(morgan('dev'));
 
+app.use(favicon(path.join('./client/dist/static','images','favicon.png')));
 app.use(express.static('./client/dist'));
 
 app.use(router);

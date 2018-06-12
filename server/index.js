b2a2527dbee8d8e36f516d067b3eff18c2476964
zaid <zaidhusseini@gmail.com>
2018-06-12 15:25:54 -0700

    Updates server to handle SSH server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const favicon = require('serve-favicon');
 const path = require('path');
 const morgan = require('morgan');
 
+const https = require('https');
+const fs = require('fs');
+
 app.use(morgan('dev'));
 
 app.use(favicon(path.join('./client/dist/static','images','favicon.png')));
@@ -12,6 +15,22 @@ app.use(express.static('./client/dist'));
 
 app.use(router);
 
-const PORT = process.env.PORT || 3000;
+const PORT_HTTP = process.env.PORT || 3000;
+const PORT_HTTPS = process.env.PORT || 4000;
 
-app.listen(PORT, ()=>console.log(`Listening on PORT ${PORT}!`));
+app.listen(PORT_HTTP, ()=>console.log(`HTTP Listening on PORT ${PORT_HTTP}!`));
+
+
+//handle SSH temporarily on test environment for lack of certs
+if (process.env.NODE_ENV === 'test'){
+
+  const httpsOptions = {
+    key: fs.readFileSync(process.env.SSL_KEY, 'utf8'),
+    cert: fs.readFileSync(process.env.SSL_CERT , 'utf8'),
+    requestCert: false,
+    rejectUnauthorized: false
+  }
+
+  https.createServer(httpsOptions, app).listen(PORT_HTTPS, () => {console.log(`HTTPS Listening on PORT ${PORT_HTTPS}!`)});
+
+}

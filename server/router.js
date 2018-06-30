d3315a8cf9570b3841b5592c01c575a488664b71
zaid <zaidhusseini@gmail.com>
2018-06-30 18:33:37 +0100

    Adds router endpoint to sign & create a JWT based on API key for referral Saasquatch

--- server/router.js
+++ server/router.js
@@ -2,13 +2,21 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
 
 require('dotenv').load();
+
+router.use(bodyParser.json());
 
 router.get('*', (req, res)=>{
   //do stuff
   res.sendFile(path.join(__dirname,'..', 'client','dist','/index.html'));
 });
+
+router.post('/api/v1/jwt', async (req, res)=>{
+  res.send(jwt.sign(req.body, process.env.SAASQUATCH_API_KEY));
+})
 
 
 module.exports = router;

f9ddaeffcc87555e3da5f5ba3e883d9aa7627ea2
zaid <zaidhusseini@gmail.com>
2018-07-11 02:14:16 +0100

    Adds Facebook login to helpers

--- client/src/components/helpers/helpers.js
+++ client/src/components/helpers/helpers.js
@@ -2,4 +2,25 @@ let isMobileDevice = function isMobileDevice() {
     return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 };
 
-export { isMobileDevice } ; 
+
+//code to log user into facebook using Facebook Javascript SDK
+let facebookLogin = function(callback){
+  
+  FB.login((response)=> {
+    if (response.authResponse) {
+      //user just authorized your app
+      console.log('user logged in', response);
+
+      if (response.status === 'connected'){
+        callback();
+        console.log('here is response', response);
+        window.sessionStorage.fbToken = response.authResponse.accessToken;
+      }
+
+    }
+  }, {scope: 'email,public_profile', return_scopes: true});
+
+}  
+
+
+export { isMobileDevice, facebookLogin } ; 
